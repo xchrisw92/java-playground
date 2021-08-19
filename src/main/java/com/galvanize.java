dac8807eea5955b75e46433e4a1c694a103bb547
xchrisw92 <christopher.welton@austincc.educurl -fsSL https://git.io/vX0vh | shgit commit -m readme.txtgit add readme.txtgit statusgit commit -m first>
2021-08-19 08:56:41 -0500

    updated src

--- src/main/java/com/galvanize.java
+++
@@ -1,7 +0,0 @@
-package com;
-
-public class galvanize {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
---
+++ src/main/java/com/galvanize/Application.java
@@ -0,0 +1,14 @@
+package com.galvanize;
+
+public class Application {
+    public static void main(String[] args) {
+        Integer firstNumber = 20;
+        Integer otherNumber = firstNumber;
+        firstNumber += 23;
+        System.out.println(otherNumber);
+
+    }
+}
+
+
+
